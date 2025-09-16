5b7e4f89a5f9cd84b863dc2b0a8cae46d7e12ffc
ShengQing521 <3313398774@qq.com>
2025-09-16 14:25:14 +0800

    Remove duplicate import statement in ProducerConsumerController

--- Scheduler/src/main/java/com/example/producer/controller/ProducerConsumerController.java
+++ Scheduler/src/main/java/com/example/producer/controller/ProducerConsumerController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/producer-consumer")
